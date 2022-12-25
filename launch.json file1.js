fc1bb6a638a0b4b51767c642e92f385ebc5b8e3b
Petah Hillcrest <peterochieng008@gmail.com>
2022-12-25 12:38:54 +0300

    create project.html file

---
+++ launch.json file1.js
@@ -0,0 +1,31 @@
+// Write a message to the console.
+console.log('hello world!')
+console.log('My name is Peter Ochieng')
+const myFirstVariable = 250
+console.log(myFirstVariable)
+//for strings use either single or double quotes
+//For Booleans and integers do not use either of the quotes
+const myage=56
+const imoverage=true
+const name='Hillcrest Petah'
+const bigIntNumber=8756453456789
+console.log(myage)
+console.log(name)
+console.log(imoverage)
+console.log(bigIntNumber)
+    //JS allows one to store an arrays of different data types.
+    //typeof operators
+    //in javascript arrays are objects
+    //numbers
+    let average
+    let sum=200
+    let n=5
+    average=sum/n
+    console.log(average)
+    //JS Functions
+    function makeconsole(){
+        console.log('I am happy this Christmas day morning.')
+    }
+makeconsole()
+//passing parameters to a function
+
